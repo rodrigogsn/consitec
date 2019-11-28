54c0600478eaf0847b7dc3b0da0e9935df5acddc
guto2xx <guto2xx@gmail.com>
2019-11-28 08:24:18 -0300

    projetos

--- src/views/projetos/projetos.php
+++ src/views/projetos/projetos.php
@@ -1,6 +1,28 @@
-<?php $title = "Gestão de Projetos" ?>
+<?php 
+  $title = "Gestão de Projetos";
+  $projetos_path = realpath(__DIR__."/../../projetos");
+  $projetos = scandir($projetos_path);
+?>
 <?php include "./src/views/includes/head.php" ?>
 
-<h1>grade de projetos</h1>
+<div class='projetos_container'>
+  <?php
+    foreach ($projetos as &$value) {
+      if($value !== "." && $value !== ".."){
+        $projeto_path = realpath($projetos_path."/".$value);
+        $projeto_first_img = scandir($projeto_path)[2];
+
+        echo("
+          <div class='projeto' style='background-image:url(\"./src/projetos/{$value}/{$projeto_first_img}\")'>
+            <span>
+              <label class='title'>{$value}</label>
+              <label class='type'>Construção civil</label>
+            </span>
+          </div>
+          ");
+      }
+    }
+  ?>
+</div>
 
 <?php include "./src/views/includes/footer.php" ?>
