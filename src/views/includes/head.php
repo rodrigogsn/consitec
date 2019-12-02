1ea255cef0992ca69d0c947bb46e272b6bcf150f
Rodrigo Gomes <rodrigogsn@gmail.com>
2019-12-02 11:41:57 -0300

    Meta tags and favicon

--- src/views/includes/head.php
+++ src/views/includes/head.php
@@ -10,7 +10,32 @@
   <meta http-equiv="Content-Language" content="pt-br">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <meta name="description" content="A CONSITEC tem o objetivo de fornecer as melhores práticas e soluções nas áreas da engenharia e tecnologia. É uma das construtoras mais conceituadas do Brasil.">
+  <meta name="keywords" content="consitec, engenharia, construtora, tecnologia, construção, civil, elétrica, operacional, infraestruturas, subterrâneas, pré-construção">
+  <meta name="robots" content="">
+  <meta name="revisit-after" content="1 day">
+  <meta name="language" content="Portuguese">
+  <meta name="generator" content="N/A">
   <title><?= $title ?></title>
+
+  <link rel="apple-touch-icon" sizes="57x57" href="./src/public/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="./src/public/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="./src/public/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="./src/public/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="./src/public/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="./src/public/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="./src/public/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="./src/public/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="./src/public/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="./src/public/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./src/public/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="./src/public/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./src/public/favicon-16x16.png">
+  <link rel="manifest" href="./src/public/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+  <meta name="theme-color" content="#003b4a">
 
   <link rel="stylesheet" type="text/css" href="./src/assets/css/main.css"/>
   <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
